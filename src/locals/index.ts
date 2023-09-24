f7c1878c4883f114f55375ceef7a3fe560eb0e3b
owl-lei <343907013@qq.com>
2023-09-24 14:22:23 +0800

    build：配置i18n插件

--- src/locals/index.ts
+++ src/locals/index.ts
@@ -1,17 +1,17 @@
-import { createI18n } from 'vue-i18n'
-import zh from './zh'
-import en from './en'
+import { createI18n } from 'vue-i18n';
+import zh from './zh.json';
+import en from './en.json';
 
 const messages = {
-  en,
-  zh,
-}
-const language = (navigator.language || 'en').toLocaleLowerCase() // 这是获取浏览器的语言
+  en: { ...en },
+  zh: { ...zh },
+};
+const language = (navigator.language || 'en').toLocaleLowerCase(); // 这是获取浏览器的语言
 const i18n = createI18n({
   locale: localStorage.getItem('lang') || language.split('-')[0] || 'en', // 首先从缓存里拿，没有的话就用浏览器语言，
   fallbackLocale: 'en', // 设置备用语言
-  messages, 
-  legacy: false
-})
+  messages,
+  legacy: false,
+});
 
-export default i18n
+export default i18n;
