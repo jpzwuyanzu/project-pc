326cb697daafb99e9f90b4385dc5285ff3a05ee6
ximeng <peterliloveyou@gmail.com>
2023-09-24 09:55:09 +0700

    固定版心

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="page_contaier">
     <HeaderCom />
-    <Banner/>
     <RouterView />
     <FooterCom />
   </div>
@@ -9,12 +8,12 @@
 <script setup lang="ts">
 import HeaderCom from "@/components/Header.vue";
 import FooterCom from "@/components/Footer.vue";
-import Banner from "@/components/Banner.vue"
 </script>
 
 <style lang="scss" scoped>
 .page_contaier {
-  width: 100%;
+  width: 1200px;
+  margin: 0 auto;
   height: 100%;
 }
 </style>
